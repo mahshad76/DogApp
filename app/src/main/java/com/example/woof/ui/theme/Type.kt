22b056621f4b0703143440b77ae503e0b12e1521
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-05-20 17:21:39 -0400

    changing the theme file

--- app/src/main/java/com/example/woof/ui/theme/Type.kt
+++ app/src/main/java/com/example/woof/ui/theme/Type.kt
@@ -2,8 +2,31 @@ package com.example.woof.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.woof.R
 
-val AppTypography = Typography()
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_regular),
+    Font(R.font.montserrat_bold, FontWeight.Bold)
+)
+
+val AppTypography = Typography(
+    displayMedium = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    )
+)
